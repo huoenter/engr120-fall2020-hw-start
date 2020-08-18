0218153619aa4ad05f53bbe5004ce6f9e8752a0a
Chen Huo <chuo@cs.ship.edu>
2020-08-17 23:08:51 -0400

    draft autograder. make it pretty?

--- hw1/main.c
+++ hw1/main.c
@@ -3,6 +3,20 @@
 int add(int a, int b);
 
 int main(void) {
-  printf("add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("FAILED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("FAILED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("FAILED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
+  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
   return 0;
 }
