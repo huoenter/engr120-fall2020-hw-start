1f41fc4c24d75164b4d25c26356b822219bf8644
Chen Huo <chuo@cs.ship.edu>
2020-08-20 15:30:58 -0400

    update

--- hw1/main.c
+++ hw1/main.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 
+int test_int(int e, int a, char *am)
+{
+    if (e == a) {
+        printf("PASSED\t%s should be %d\n", am, e);
+        return 1;
+    } else {
+        printf("FAILED\t%s should be %d, your answer is %d\n", am, e, a);
+        return 0;
+    }
+}
+
 int add(int a, int b);
 
 int main(void) {
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("FAILED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("FAILED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("FAILED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  printf("PASSED add(3,4) should be 7. Result is %d.", add(3, 4));
-  return 0;
+    test_int(add(3, 4), 0, "add(3,4)");
+    test_int(add(0, 0), 0, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    test_int(add(3, 4), 0, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    test_int(add(3, 4), 0, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    test_int(add(3, 4), 0, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    test_int(add(3, 4), 7, "add(3,4)");
+    return 0;
 }
